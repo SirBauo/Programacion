b57d472d000988bc29f0c37c0ec9c04568aeb4ed
Erick <erick.r94@hotmail.com>
2025-09-22 18:42:54 -0600

    first commit

---
+++ Week 1/Clase 1.py
@@ -0,0 +1,14 @@
+x = 1
+y = 2.8
+z = 1j
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+#------------#
+
+x = 1
+x = 2
+#La siguiente linea al intentar ejecutarse tirea error
+#print("La X vale " + x + ", y la Y vale " + y)
---
+++ Week 2/Quiz.py
@@ -0,0 +1,11 @@
+numero = 5
+contador = 0
+
+for contador in range(0, numero):
+
+    if contador % 2 ==0:
+        print(f"{numero}")
+    else:
+        break
+
+lista =
